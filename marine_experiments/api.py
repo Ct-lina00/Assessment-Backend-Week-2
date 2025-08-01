25e252627a829a8ffb7c7f222c8cb416e183f3ac
catalina <trainee.catalina.creanga@sigmalabs.co.uk>
2025-08-01 17:03:49 +0100

    task 1 and 2

--- marine_experiments/api.py
+++ marine_experiments/api.py
@@ -5,13 +5,13 @@ from datetime import datetime
 from flask import Flask, jsonify, request
 from psycopg2 import sql
 
-from database_functions import get_db_connection
+from database_functions import get_db_connection, get_subjects, get_experiments
 
 
 app = Flask(__name__)
 
 """
-For testing reasons; please ALWAYS use this connection. 
+For testing reasons; please ALWAYS use this connection.
 
 - Do not make another connection in your code
 - Do not close this connection
@@ -19,6 +19,16 @@ For testing reasons; please ALWAYS use this connection.
 If you do not understand this instructions; as a coach to explain
 """
 conn = get_db_connection("marine_experiments")
+
+
+def validate_type(type):
+    """Return if type valid for the request."""
+    return type in {"intelligence", "obedience", "aggression"}
+
+
+def validate_score_over(score_over):
+    """Return if score is valid for the request."""
+    return score_over in range[100]
 
 
 @app.get("/")
@@ -29,6 +39,65 @@ def home():
         "resource": "JSON-based API",
         "status": "Classified"
     })
+
+
+@app.route("/subject", methods=["GET"])
+def get_subject_endpoint():
+    """Function executes the GET request returning list of subject objects. """
+    subjects = get_subjects(conn)
+    for s in subjects:
+        s["date_of_birth"] = datetime.strftime(s["date_of_birth"], "%Y-%m-%d")
+    if subjects == []:
+        return subjects
+    if not subjects:
+        return {"error": "No subjects found"}, 404
+    return subjects, 200
+
+
+@app.route("/experiment", methods=["GET"])
+def get_experiment_endpoint():
+    """Function executes the GET request returning list of experiment objects. """
+    if request.method == "GET":
+        type = None
+        score_over = None
+        if "type" in request.args:
+            type = request.args.get("type")
+            if type is not None and not validate_type(type):
+                return {"error": "Invalid value for type parameter"}, 400
+
+        if "score_over" in request.args:
+            score_over = request.args.get("score_over")
+            try:
+                score_over = int(score_over)
+                if score_over is not None and not validate_score_over(score_over):
+                    return {"error": "Invalid value for score_over parameter"}, 400
+            except TypeError:
+                return {"error": "Invalid type value for score_over parameter"}, 400
+
+        experiments = get_experiments(conn, type, score_over)
+        for s in experiments:
+            s["experiment_date"] = datetime.strftime(
+                s["experiment_date"], "%Y-%m-%d")
+
+        if experiments == []:
+            return experiments
+
+        if not experiments:
+            return {"error": "No experiments found"}, 404
+
+        return experiments, 200
+
+
+@app.route("/experiment/<id>", methods=["DELETE"])
+def delete_experiment_endpoint():
+    """Function executes the delete request. """
+
+    success = delete_experiment(conn, experiment_id)
+
+    if not success:
+        return {"error": "Movie could not be deleted"}, 404
+
+    return {"message": "Movie deleted"}
 
 
 if __name__ == "__main__":
--- marine_experiments/database_functions.py
+++ marine_experiments/database_functions.py
@@ -1,6 +1,6 @@
 """Functions that interact with the database."""
 
-from psycopg2 import connect
+from psycopg2 import connect, sql
 from psycopg2.extras import RealDictCursor
 from psycopg2.extensions import connection
 
@@ -14,3 +14,60 @@ def get_db_connection(dbname,
                    port=5432,
                    password=password,
                    cursor_factory=RealDictCursor)
+
+
+def get_subjects(conn: connection) -> list[dict]:
+    """This function returns all subjects in the marine_experiments database."""
+    with conn.cursor() as cursor:
+        query = """
+        SELECT
+        s.date_of_birth, sp.species_name, s.subject_id, s.subject_name FROM subject s
+        JOIN species sp
+        ON sp.species_id = s.species_id
+        ORDER BY s.date_of_birth DESC;"""
+        cursor.execute(query)
+
+        rows = cursor.fetchall()
+    return rows
+
+
+def get_experiments(conn: connection, type: str = None, score_over: int = None) -> list[dict]:
+    """This function returns all subjects in the marine_experiments database."""
+    with conn.cursor() as cursor:
+        query = sql.SQL("""
+            SELECT
+            e.experiment_id, e.subject_id,
+            e.experiment_date, sp.species_name as species, et.type_name as experiment_type,
+            CONCAT(ROUND(e.score * (100/ et.max_score), 2), '%') AS Score
+            FROM experiment e
+            JOIN subject s
+            USING (subject_id)
+            JOIN species sp
+            USING (species_id)
+            JOIN experiment_type et
+            ON et.experiment_type_id = e.experiment_type_id
+            ORDER BY e.experiment_date DESC;""")
+
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+    return rows
+
+
+def delete_experiment(conn: connection, experiment_id: str):
+    with conn.cursor() as cursor:
+        query = """
+        SELECT experiment_id, experiment_date
+        FROM experiment
+        WHERE experiment_id = %(experiment_id)s;"""
+        cursor.execute(query, {"experiment_id": experiment_id})
+        rows = cursor.fetchone()
+        if rows:
+            query2 = """
+                    DELETE FROM experiment
+                    WHERE experiment_id = %(experiment_id)s; """
+            cursor.execute(query2, {"experiment_id": experiment_id})
+            conn.commit()
+            return rows, 200
+        if not rows:
+            return {"error": f"Unable to locate experiment with ID {experiment_id}."}, 404
